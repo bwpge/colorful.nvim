40e96325e804daad69c2d4da71809e2762100f5b
bwpge <bwpge.dev@gmail.com>
2024-03-06 13:26:23 -0500

    Add HSL functions and refactor color methods

--- lua/colorful/color.lua
+++ lua/colorful/color.lua
@@ -11,9 +11,11 @@ Color.__tostring = function(self)
 end
 
 ---Create a new color object.
----@param r integer Red channel value
----@param g integer Green channel value
----@param b integer Blue channel value
+---
+---RGB components must be normalized to floating point values between 0-1.
+---@param r number Normalized red component
+---@param g number Normalized green component
+---@param b number Normalized blue component
 ---@return Color
 function Color:new(r, g, b)
     local o = {
@@ -22,32 +24,45 @@ function Color:new(r, g, b)
         b = b,
     }
     for k, val in pairs(o) do
-        if not u.in_range(val, 0, 255) then
-            error(string.format("RGB channel values must be between 0-255 (got %s=%s)", k, val))
+        if not u.in_range(val, 0, 1) then
+            error(string.format("RGB component values must be between 0-1 (got %s=%f)", k, val))
         end
     end
 
     return setmetatable(o, self)
 end
 
----Parses a color from a string.
----@param s string A hexadecimal string with the form `#RGB` or `#RRGGBB`
+---Parses a color from a string or number.
+---
+--- - Strings must have the form `#RGB` or `#RRGGBB`
+--- - Numbers must be decimal representations of the above hexadecimal forms
+---   (e.g., `1194684` for `#123abc`, or `1122867` for `#123`)
+---@param value string|number
 ---@return Color
-function Color:parse(s)
-    local value = s:match("^#%x%x%x%x%x%x$") or s:match("^#%x%x%x$")
-    if not value then
+function Color:parse(value)
+    local s
+    if type(value) == "string" then
+        s = value
+    elseif type(value) == "number" then
+        s = string.format("#%06x", value)
+    else
+        error(string.format("Cannot parse a color from type `%s`", type(value)))
+    end
+
+    local val = s:match("^#%x%x%x%x%x%x$") or s:match("^#%x%x%x$")
+    if not val then
         error(string.format("`%s` is not a valid hexadecimal color", s))
     end
 
     local color = {}
-    if value:len() == 4 then
-        color.r = (tonumber(value:sub(2, 2), 16) * 17)
-        color.g = (tonumber(value:sub(3, 3), 16) * 17)
-        color.b = (tonumber(value:sub(4, 4), 16) * 17)
+    if val:len() == 4 then
+        color.r = (tonumber(val:sub(2, 2), 16) * 17) / 255
+        color.g = (tonumber(val:sub(3, 3), 16) * 17) / 255
+        color.b = (tonumber(val:sub(4, 4), 16) * 17) / 255
     else
-        color.r = tonumber(value:sub(2, 3), 16)
-        color.g = tonumber(value:sub(4, 5), 16)
-        color.b = tonumber(value:sub(6, 7), 16)
+        color.r = tonumber(val:sub(2, 3), 16) / 255
+        color.g = tonumber(val:sub(4, 5), 16) / 255
+        color.b = tonumber(val:sub(6, 7), 16) / 255
     end
 
     return setmetatable(color, self)
@@ -56,56 +71,71 @@ end
 ---Returns this color as a hexadecimal string with the form `#RRGGBB`.
 ---@return string
 function Color:hex()
-    return string.format("#%02x%02x%02x", self.r, self.g, self.b)
+    local r = u.round(self.r * 255)
+    local g = u.round(self.g * 255)
+    local b = u.round(self.b * 255)
+    return string.format("#%02x%02x%02x", r, g, b)
+end
+
+---Returns this color as a tuple of HSL components.
+---
+---All components returned are normalized in the range [0, 1].
+---@nodiscard
+---@return number h, number s, number l Hue, saturation, and lightness components
+function Color:hsl()
+    return u.rgb_to_hsl(self.r, self.g, self.b)
 end
 
 ---Applies a linear blend between this color and `rhs`, where `amount` is used
 ---from this color and `1 - amount` is used of `rhs`.
 ---@param rhs Color
----@param amount number A floating point value between 0 and 1
+---@param amount number A floating point value in the range [0, 1]
 ---@return Color
 function Color:blend(rhs, amount)
     if amount > 1 or amount < 0 then
-        error("blend amount must be between 0 and 1")
+        error("blend amount must be in the range [0, 1]")
     end
 
     local f = 1.0 - amount
-    self.r = u.round(u.clamp((amount * self.r) + (f * rhs.r), 0, 255))
-    self.g = u.round(u.clamp((amount * self.g) + (f * rhs.g), 0, 255))
-    self.b = u.round(u.clamp((amount * self.b) + (f * rhs.b), 0, 255))
+    self.r = u.clamp((amount * self.r) + (f * rhs.r), 0, 1)
+    self.g = u.clamp((amount * self.g) + (f * rhs.g), 0, 1)
+    self.b = u.clamp((amount * self.b) + (f * rhs.b), 0, 1)
 
     return self
 end
 
----Lightens the color by adding `amount * (255 - channel)` of each RGB channel.
----@param amount number A floating point value between 0 and 1
+---Lightens this color by adding `amount` to the lightness.
+---
+---This is somewhat computationally expensive, as the RGB components are converted to HSL
+---to preserve the base hue and saturation.
+---@param amount number A floating point value in the range [0, 1]
 ---@return Color
-function Color:lighten(amount)
-    if amount > 1 or amount < 0 then
-        error("blend amount must be between 0 and 1")
+function Color:tint(amount)
+    if not u.in_range(amount, 0, 1) then
+        error("tint amount must be in the range [0, 1]")
     end
 
-    local dr = amount * (255 - self.r)
-    local dg = amount * (255 - self.g)
-    local db = amount * (255 - self.b)
-    self.r = u.round(u.clamp(self.r + dr, 0, 255))
-    self.g = u.round(u.clamp(self.g + dg, 0, 255))
-    self.b = u.round(u.clamp(self.b + db, 0, 255))
+    local h, s, l = self:hsl()
+    l = u.clamp(l + amount, 0, 1)
+    self.r, self.g, self.b = u.hsl_to_rgb(h, s, l)
 
     return self
 end
 
----Darkens the color by subtracting `amount * channel` of each RGB channel.
----@param amount number A floating point value between 0 and 1
+---Darkens this color by subtracting `amount` from the lightness.
+---
+---This is somewhat computationally expensive, as the RGB components are converted to HSL
+---to preserve the base hue and saturation.
+---@param amount number A floating point value in the range [0, 1]
 ---@return Color
-function Color:darken(amount)
-    if amount > 1 or amount < 0 then
-        error("blend amount must be between 0 and 1")
+function Color:shade(amount)
+    if not u.in_range(amount, 0, 1) then
+        error("shade amount must be in the range [0, 1]")
     end
 
-    self.r = u.round(u.clamp(self.r - (amount * self.r), 0, 255))
-    self.g = u.round(u.clamp(self.g - (amount * self.g), 0, 255))
-    self.b = u.round(u.clamp(self.b - (amount * self.b), 0, 255))
+    local h, s, l = self:hsl()
+    l = u.clamp(l - amount, 0, 1)
+    self.r, self.g, self.b = u.hsl_to_rgb(h, s, l)
 
     return self
 end
--- lua/colorful/utils.lua
+++ lua/colorful/utils.lua
@@ -9,20 +9,135 @@ function M.clamp(value, min, max)
     return math.max(math.min(value, max), min)
 end
 
----Rounds a number to the nearest integer.
----@param value number
+---Rounds a number to the nearest integer, or to the specified number of decimal places.
+---@param value number The value to round
+---@param places? integer Optional number of decimal places (must be positive if used)
 ---@return integer
-function M.round(value)
-    return math.floor(value + 0.5)
+function M.round(value, places)
+    if not places then
+        return math.floor(value + 0.5)
+    end
+
+    if places < 1 then
+        error("Cannot round to negative decimal places")
+    end
+    local b = 10 ^ math.floor(places)
+    return math.floor(value * b + 0.5) / b
+end
+
+---Returns the minimum and maximum values of the given arguments.
+---@param x number
+---@param ... number
+---@return number min, number max
+function M.minmax(x, ...)
+    return math.min(x, ...), math.max(x, ...)
 end
 
 ---Returns whether or not a number is in a given range.
----@param value number
----@param min number
----@param max number
+---@param value number The value to be checked
+---@param min number The minimum value, inclusive
+---@param max number The maximum value, inclusive
 ---@return boolean
 function M.in_range(value, min, max)
     return value >= min and value <= max
+end
+
+---Converts RGB values to HSL.
+---
+---Components must be normalized to floating point values in the range [0, 1].
+---
+---Adapted from https://github.com/EmmanuelOga/columns/blob/836312be76b85b7f85e0cb2c31f5f22624c62d3e/utils/color.lua
+---It seems this implementation originated from: https://stackoverflow.com/a/9493060
+---@param r number The normalized red component
+---@param g number The normalized green component
+---@param b number The normalized blue component
+---@return number, number, number
+function M.rgb_to_hsl(r, g, b)
+    for _, component in ipairs({ r, g, b }) do
+        if not M.in_range(component, 0, 1) then
+            error("RGB components must be normalized to the range [0-1]")
+        end
+    end
+
+    local h, s, l
+    local min, max = M.minmax(r, g, b)
+    l = (max + min) / 2
+
+    -- achromatic
+    if max == min then
+        return 0, 0, l
+    end
+
+    local chroma = max - min
+    if l > 0.5 then
+        s = chroma / (2 - max - min)
+    else
+        s = chroma / (max + min)
+    end
+    if max == r then
+        h = (g - b) / chroma
+        if g < b then
+            h = h + 6
+        end
+    elseif max == g then
+        h = (b - r) / chroma + 2
+    elseif max == b then
+        h = (r - g) / chroma + 4
+    end
+    h = h / 6
+
+    return h, s, l
+end
+
+---Converts HSL values to RGB.
+---
+---Components must be normalized to floating point values in the range [0, 1].
+---
+---Adapted from https://github.com/EmmanuelOga/columns/blob/836312be76b85b7f85e0cb2c31f5f22624c62d3e/utils/color.lua
+---It seems this implementation originated from: https://stackoverflow.com/a/9493060
+---@param h number
+---@param s number
+---@param l number
+---@return number, number, number
+function M.hsl_to_rgb(h, s, l)
+    -- achromatic
+    if s == 0 then
+        return l, l, l
+    end
+
+    local function hue_to_rgb(p, q, t)
+        if t < 0 then
+            t = t + 1
+        end
+        if t > 1 then
+            t = t - 1
+        end
+        if t < 1 / 6 then
+            return p + (q - p) * 6 * t
+        end
+        if t < 1 / 2 then
+            return q
+        end
+        if t < 2 / 3 then
+            return p + (q - p) * (2 / 3 - t) * 6
+        end
+        return p
+    end
+
+    local q
+    if l < 0.5 then
+        q = l * (1 + s)
+    else
+        q = l + s - l * s
+    end
+    local p = 2 * l - q
+
+    local r, g, b
+    r = hue_to_rgb(p, q, h + 1 / 3)
+    g = hue_to_rgb(p, q, h)
+    b = hue_to_rgb(p, q, h - 1 / 3)
+
+    return r, g, b
 end
 
 return M
