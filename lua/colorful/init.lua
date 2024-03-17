3d6c68c14dc00ca22cf1cc0b6e1507e436298245
bwpge <bwpge.dev@gmail.com>
2024-03-16 21:46:52 -0400

    Add plugin setup with user options

--- lua/colorful/init.lua
+++ lua/colorful/init.lua
@@ -1,9 +1,57 @@
+local Highlight = require("colorful.highlight")
+
 local M = {}
 
-local color = require("colorful.color")
+---@alias HighlightTable table<string, table> | fun(): table<string, table>
 
-M.RGBColor = color.RGBColor
-M.HSLColor = color.HSLColor
-M.Highlight = require("colorful.highlight")
+---@class ColorfulConfig
+---@field highlights HighlightTable
+---@field create_autocmd boolean
+---@field apply_on_setup boolean
+
+---@class ColorfulUserConfig
+---@field highlights? HighlightTable
+---@field create_autocmd? boolean
+---@field apply_on_setup? boolean
+
+---@type ColorfulConfig
+M.default_config = {
+    highlights = {},
+    create_autocmd = true,
+    apply_on_setup = true,
+}
+
+---@type ColorfulConfig
+---@diagnostic disable-next-line: missing-fields
+M.options = {}
+
+local function apply_highlights()
+    local t
+    if type(M.options.highlights) == "function" then
+        t = M.options.highlights()
+    else
+        t = M.options.highlights
+        ---@cast t table<string, table>
+    end
+
+    for k, v in pairs(t) do
+        Highlight.new(v):set(k)
+    end
+end
+
+---Runs the plugin setup with user provided options.
+---@param opts? ColorfulUserConfig
+function M.setup(opts)
+    M.options = vim.tbl_deep_extend("force", M.default_config, opts or {})
+
+    if M.options.create_autocmd then
+        vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+            callback = apply_highlights,
+        })
+    end
+    if M.options.apply_on_setup then
+        apply_highlights()
+    end
+end
 
 return M
