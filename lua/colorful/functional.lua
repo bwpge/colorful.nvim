1e0ffce188f99c405d34bac69fcaa82cfbfa0e6f
bwpge <bwpge.dev@gmail.com>
2024-03-18 20:11:56 -0400

    Add more color closures

--- lua/colorful/functional.lua
+++ lua/colorful/functional.lua
@@ -14,9 +14,33 @@ function M.map(val, fn)
 end
 
 ---Returns a closure (function) that takes a single `Color` input and returns the result of
+---`Color:adjust_rgb(...)`.
+---@param r number
+---@param g number
+---@param b number
+---@return Color.MapFn
+function M.adjust_rgb(r, g, b)
+    return function(c)
+        return c:adjust_rgb(r, g, b)
+    end
+end
+
+---Returns a closure (function) that takes a single `Color` input and returns the result of
+---`Color:adjust_hsl(...)`.
+---@param h number
+---@param s number
+---@param l number
+---@return Color.MapFn
+function M.adjust_hsl(h, s, l)
+    return function(c)
+        return c:adjust_hsl(h, s, l)
+    end
+end
+
+---Returns a closure (function) that takes a single `Color` input and returns the result of
 ---`Color:rotate(...)`.
 ---@param amount number
----@return fun(c: Color): Color
+---@return Color.MapFn
 function M.rotate(amount)
     return function(c)
         return c:rotate(amount)
@@ -26,7 +50,7 @@ end
 ---Returns a closure (function) that takes a single `Color` input and returns the result of
 ---`Color:saturate(...)`.
 ---@param amount number
----@return fun(c: Color): Color
+---@return Color.MapFn
 function M.saturate(amount)
     return function(c)
         return c:saturate(amount)
@@ -36,7 +60,7 @@ end
 ---Returns a closure (function) that takes a single `Color` input and returns the result of
 ---`Color:lighten(...)`.
 ---@param amount number
----@return fun(c: Color): Color
+---@return Color.MapFn
 function M.lighten(amount)
     return function(c)
         return c:lighten(amount)
