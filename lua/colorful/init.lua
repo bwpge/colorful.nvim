e592a0cc1f6ff383b1493f4a4ffadb1db3838899
bwpge <bwpge.dev@gmail.com>
2024-03-18 20:12:44 -0400

    Refactor plugin setup and user config

--- lua/colorful/init.lua
+++ lua/colorful/init.lua
@@ -2,7 +2,8 @@ local Highlight = require("colorful.highlight")
 
 local M = {}
 
----@alias HighlightTable table<string, table> | fun(): table<string, table>
+---@alias PatternTable table<string, table>
+---@alias HighlightTable fun(): table<string, PatternTable>
 
 ---@class ColorfulConfig
 ---@field highlights HighlightTable
@@ -16,7 +17,9 @@ local M = {}
 
 ---@type ColorfulConfig
 M.default_config = {
-    highlights = {},
+    highlights = function()
+        return {}
+    end,
     create_autocmd = true,
     apply_on_setup = true,
 }
@@ -25,17 +28,26 @@ M.default_config = {
 ---@diagnostic disable-next-line: missing-fields
 M.options = {}
 
-local function apply_highlights()
-    local t
-    if type(M.options.highlights) == "function" then
-        t = M.options.highlights()
-    else
-        t = M.options.highlights
-        ---@cast t table<string, table>
-    end
-
+local function set_hl_table(t)
     for k, v in pairs(t) do
         Highlight.new(v):set(k)
+    end
+end
+
+local function apply_highlights()
+    -- always apply `*` first, regardless of colorscheme
+    local hls = M.options.highlights()
+    if hls["*"] then
+        set_hl_table(hls["*"])
+        hls["*"] = nil
+    end
+
+    -- matches are not guaranteed in any particular order
+    local colorscheme = vim.g.colors_name
+    for name, t in pairs(hls) do
+        if colorscheme:match(name) then
+            set_hl_table(t)
+        end
     end
 end
 
@@ -43,6 +55,11 @@ end
 ---@param opts? ColorfulUserConfig
 function M.setup(opts)
     M.options = vim.tbl_deep_extend("force", M.default_config, opts or {})
+
+    -- nothing to do if no highlights are provided
+    if not M.options.highlights then
+        return
+    end
 
     if M.options.create_autocmd then
         vim.api.nvim_create_autocmd({ "ColorScheme" }, {
